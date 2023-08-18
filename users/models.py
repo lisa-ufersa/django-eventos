fe1dce5641a65010092483edd33385bca1e7a2c0
Ferdinandy <ferdinandsc@yahoo.com.br>
2023-08-18 14:21:59 -0300

    atualiazação nos perfis de usuários

--- users/models.py
+++ users/models.py
@@ -15,21 +15,49 @@ class UserProfileExample(models.Model):
     class Meta:
         verbose_name = "Profile"
         verbose_name_plural = "Profiles"
+    
+    def __str__(self):
+        return self.user.username
 
 class Participante(models.Model):
     certificado = models.ManyToManyField(Certificado)
     frequencias = models.CharField(max_length=200)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+
+    class Meta:
+        verbose_name = "Participante"
+        verbose_name_plural = "Participantes"
+
+    def __str__(self):
+        return 
 
 class Coordenador(models.Model):
     eventos = models.ForeignKey(Evento, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+
+    class Meta:
+        verbose_name = "Coordenador"
+        verbose_name_plural = "Coordenadores"
 
 class Ministrante(models.Model):
     minieventos = models.ForeignKey(MiniEvento, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    class Meta:
+        verbose_name = "Ministrante"
+        verbose_name_plural = "Ministrantes"
 
 class Voluntario(models.Model):
     eventos = models.ForeignKey(Evento, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    class Meta:
+        verbose_name = "Voluntario"
+        verbose_name_plural = "Voluntários"
 
 class Patrocinador(models.Model):
     nome = models.CharField(max_length=150)
     cnpj = models.CharField(max_length=100)
     eventos = models.ForeignKey(Evento, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    class Meta:
+        verbose_name = "Patrocinador"
+        verbose_name_plural = "Patrocinadores"
